d20cf506267c43fa040183e06e7449c6244321eb
Melchior Lozé <melchior.loze@epita.fr>
2022-12-21 15:18:13 +0100

    core: imported bootstrap

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,9 @@
 import { sveltekit } from '@sveltejs/kit/vite';
+import path from 'path';
 
 /** @type {import('vite').UserConfig} */
 const config = {
+	root: path.resolve(__dirname, 'src'),
 	plugins: [sveltekit()]
 };
 
