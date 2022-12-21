3fbeec5287b65be7ebe2d5d443279fb89e9775e0
Melchior Lozé <melchior.loze@epita.fr>
2022-12-21 15:25:40 +0100

    core: changed prettier configuration to have spaces instead of tabs

--- vite.config.js
+++ vite.config.js
@@ -3,8 +3,8 @@ import path from 'path';
 
 /** @type {import('vite').UserConfig} */
 const config = {
-	root: path.resolve(__dirname, 'src'),
-	plugins: [sveltekit()]
+  root: path.resolve(__dirname, 'src'),
+  plugins: [sveltekit()],
 };
 
 export default config;
