bb85754690c0f04e0982cf33c3a63b463d7f104b
Tony Heng <tony.heng@epita.fr>
2022-12-22 17:11:24 +0100

    feat: header: add navbar

--- src/routes/Header.svelte
+++ src/routes/Header.svelte
@@ -1,38 +1,47 @@
 <script>
   import { page } from '$app/stores';
-  import logo from '$lib/images/svelte-logo.svg';
-  import github from '$lib/images/github.svg';
+  import logo from '$lib/images/nile.svg';
+  const navLinks = [
+    { name: 'Home', href: '/' },
+    { name: 'Deals', href: '/deals' },
+    { name: 'Gift Cards', href: '/gift-cards' },
+    { name: 'Categories', href: '/categories' },
+    { name: 'Sell', href: '/sell' },
+  ];
 </script>
 
-<header>
-  <div class="corner">
-    <a href="https://kit.svelte.dev">
+<header class="d-flex flex-column gap-1">
+  <div class="d-flex flex-row justify-content-between p-2">
+    <a href="/" class="corner">
       <img src={logo} alt="SvelteKit" />
     </a>
-  </div>
-
-  <nav>
-    <svg viewBox="0 0 2 3" aria-hidden="true">
-      <path d="M0,0 L1,2 C1.5,3 1.5,3 2,3 L2,0 Z" />
-    </svg>
+    <form>
+      <input type="search" />
+      <button type="submit">Search</button>
+    </form>
     <ul>
-      <li aria-current={$page.url.pathname === '/' ? 'page' : undefined}>
-        <a href="/">Home</a>
+      <li>
+        <button>Profile</button>
+      </li>
+      <li>
+        <button>Cart</button>
       </li>
     </ul>
-    <svg viewBox="0 0 2 3" aria-hidden="true">
-      <path d="M0,0 L0,3 C0.5,3 0.5,3 1,2 L2,0 Z" />
-    </svg>
-  </nav>
-
-  <div class="corner">
-    <a href="https://github.com/sveltejs/kit">
-      <img src={github} alt="GitHub" />
-    </a>
   </div>
+  <nav>
+    <ul class="container">
+      {#each navLinks as link}
+        <li aria-current={$page.url.pathname === link.href}>
+          <a href={link.href}>
+            {link.name}
+          </a>
+        </li>
+      {/each}
+    </ul>
+  </nav>
 </header>
 
-<style>
+<style lang="scss">
   header {
     display: flex;
     justify-content: space-between;
@@ -58,9 +67,7 @@
   }
 
   nav {
-    display: flex;
-    justify-content: center;
-    --background: rgba(255, 255, 255, 0.7);
+    background: var(--color-accent);
   }
 
   svg {
@@ -74,33 +81,20 @@
   }
 
   ul {
-    position: relative;
-    padding: 0;
-    margin: 0;
-    height: 3em;
-    display: flex;
-    justify-content: center;
-    align-items: center;
+    margin-top: 1rem;
     list-style: none;
-    background: var(--background);
-    background-size: contain;
   }
 
-  li {
-    position: relative;
-    height: 100%;
+  ul > li {
+    display: inline-block;
+    margin: 0 0.5em;
   }
 
-  li[aria-current='page']::before {
-    --size: 6px;
-    content: '';
-    width: 0;
-    height: 0;
-    position: absolute;
-    top: 0;
-    left: calc(50% - var(--size));
-    border: var(--size) solid transparent;
-    border-top: var(--size) solid var(--color-theme-1);
+  li[aria-current='true'] a {
+    text-decoration: underline var(--color-primary-text);
+    &:hover {
+      text-decoration: underline var(--color-primary-text-hover);
+    }
   }
 
   nav a {
@@ -108,16 +102,15 @@
     height: 100%;
     align-items: center;
     padding: 0 0.5rem;
-    color: var(--color-text);
+    color: var(--color-primary-text);
     font-weight: 700;
     font-size: 0.8rem;
-    text-transform: uppercase;
-    letter-spacing: 0.1em;
     text-decoration: none;
     transition: color 0.2s linear;
   }
 
   a:hover {
-    color: var(--color-theme-1);
+    color: var(--color-primary-text-hover);
+    text-decoration: underline var(--color-primary-text-hover);
   }
 </style>
