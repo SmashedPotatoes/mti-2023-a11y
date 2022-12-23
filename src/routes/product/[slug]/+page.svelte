a98ce1613388d0ce9dfd09c211726dac818a004b
Tony Heng <tony.heng@epita.fr>
2022-12-23 15:04:36 +0100

    feat: product: add product detail page

---
+++ src/routes/product/[slug]/+page.svelte
@@ -0,0 +1,8 @@
+<script lang="ts">
+  import { page } from '$app/stores';
+</script>
+
+<h1>{$page.data.product.name}</h1>
+
+<style>
+</style>
---
+++ src/routes/product/[slug]/+page.ts
@@ -0,0 +1,10 @@
+import { getProducts } from '../../../lib/services/Products';
+import type { PageLoadEvent } from './$types';
+
+export function load(event: PageLoadEvent) {
+  const products = getProducts();
+  const product = products.find((product) => product.name === event.params.slug);
+  return {
+    product,
+  };
+}
