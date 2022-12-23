5889927d60a6497231ea2f374c49ae34fc79a52e
Melchior Lozé <melchior.loze@epita.fr>
2022-12-23 02:15:16 +0100

    refacto: created component for add to cart button

---
+++ src/routes/AddToCartButton.svelte
@@ -0,0 +1,23 @@
+<script lang="ts">
+  import addToCartIcon from '$lib/images/add-to-cart.svg';
+
+  export let onClick: () => void = () => {};
+</script>
+
+<button class="overlap add-to-cart-btn" on:click={onClick}>
+  <img src={addToCartIcon} alt="add to cart" />
+</button>
+
+<style>
+  .add-to-cart-btn {
+    background-color: var(--color-call-to-action);
+    border: none;
+    border-radius: 100%;
+    padding: 0.5rem;
+    width: 2.5rem;
+    height: 2.5rem;
+    top: -0.4rem;
+    right: -0.4rem;
+    left: auto;
+  }
+</style>
--- src/routes/DealProduct.svelte
+++ src/routes/DealProduct.svelte
@@ -1,7 +1,7 @@
 <script lang="ts">
-  import addToCartIcon from '$lib/images/add-to-cart.svg';
   import type { DealProductProps } from '$lib/interfaces/products/deal-product-props';
   import { priceToText } from '$lib/utils/price-to-text';
+  import AddToCartButton from './AddToCartButton.svelte';
 
   export let product: DealProductProps;
   export let href: string = '/';
@@ -22,9 +22,7 @@
     </div>
     <p class="deal-percentage">{product.deal}% off</p>
   </div>
-  <button class="overlap add-to-cart-btn" on:click={product.onAddToCart}>
-    <img src={addToCartIcon} alt="add to cart" />
-  </button>
+  <AddToCartButton onClick={product.onAddToCart} />
 </li>
 
 <style>
@@ -60,17 +58,5 @@
     padding: 0.2rem;
     width: fit-content;
     margin: 0;
-  }
-
-  .add-to-cart-btn {
-    background-color: var(--color-call-to-action);
-    border: none;
-    border-radius: 100%;
-    padding: 0.5rem;
-    width: 2.5rem;
-    height: 2.5rem;
-    top: -0.4rem;
-    right: -0.4rem;
-    left: auto;
   }
 </style>
